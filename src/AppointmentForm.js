485e3b3d193c371f9dd570722a59c0ddfbb2a282
Atif Hafeez <atif.hafeez@transcore.com>
2023-02-25 13:04:02 +0400

    Adding a header row

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -1,21 +1,88 @@
 import React from "react";
 
+const timeIncrements = (
+  numTimes,
+  startTime,
+  increment
+) => 
+  Array(numTimes)
+    .fill([startTime])
+    .reduce((acc, _, i) => 
+      acc.concat([startTime + i * increment])  
+    );
+
+const dailyTimeSlots = (
+  salonOpensAt,
+  salonClosesAt
+) => {
+  const totalSlots = 
+    (salonClosesAt - salonOpensAt) * 2;
+  const startTime = new Date()
+    .setHours(salonOpensAt, 0, 0, 0);
+  const increment = (30 * 60 * 1000);
+  console.log(totalSlots);
+  return timeIncrements(
+    totalSlots,
+    startTime,
+    increment
+  );
+}
+
+const toTimeValue = timestamp => 
+  new Date(timestamp).toTimeString().substring(0,5);
+
+const TimeSlotTable = (
+  {salonOpensAt,
+  salonClosesAt}
+) => {
+  const timeSlots = dailyTimeSlots (
+    salonOpensAt,
+    salonClosesAt
+  );
+
+  return (
+    <table id="time-slots">
+      <tbody>
+        {timeSlots.map(timeSlot => (
+          <tr key={timeSlot}>
+            <th>{toTimeValue(timeSlot)}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>  
+  );
+};
+
+
 export const AppointmentForm = ({
-  selectableServices
+  original,
+  selectableServices,
+  salonOpensAt,
+  salonClosesAt,
 }) => {
   return (
     <form>
-      <select name="service">
+      <select 
+        name="service"
+        value={original.service}
+        readOnly
+      >
         <option />
         {selectableServices.map(s => (
           <option key={s}>{s}</option>
         ))}
       </select>
+      <TimeSlotTable 
+          salonOpensAt={salonOpensAt}
+          salonClosesAt={salonClosesAt}
+      />
     </form>
   );
 }
 
 AppointmentForm.defaultProps = {
+  salonOpensAt: 9,
+  salonClosesAt: 19,
   selectableServices: [
     "Cut", 
     "Blow-dry",
--- test/AppointmentForm.test.js
+++ test/AppointmentForm.test.js
@@ -4,10 +4,26 @@ import {
   render,
   field,
   form,
-} from "./reactTestExtensions"
+  element,
+  elements,
+} from "./reactTestExtensions";
+import { toContainText } from "./matchers/toContainText";
 import {AppointmentForm} from "../src/AppointmentForm";
 
+const findOption = (selectBox, textContent) => {
+  const options = Array.from(selectBox.childNodes);
+  return options.find(
+    option => option.textContent === textContent
+  );
+};
+
 describe("AppointmentForm", () => {
+
+  const blankAppointment = {
+    service: ""
+  };
+
+  const services = ["Cut", "Blow-dry"];
 
   beforeEach(()=> {
     initializeReactContainer();
@@ -20,36 +36,96 @@ describe("AppointmentForm", () => {
     );
   
   it("renders a form", () => {
-    render(<AppointmentForm />);
+    render(
+      <AppointmentForm 
+        original={blankAppointment}
+      />
+    );
     expect(form()).not.toBeNull();
   })
 
   describe("service field", () => {
-
-
-  
+ 
     it("renders as a select box", () => {
-      render(<AppointmentForm />);
+      render(
+        <AppointmentForm 
+          original={blankAppointment}
+        />
+      );
       expect(field("service")).not.toBeNull();
       expect(field("service").tagName).toEqual("SELECT");
     })
     it("has a blank value as the first value", () => {
-      render(<AppointmentForm />);
+      render(
+        <AppointmentForm 
+          original={blankAppointment}
+        />
+      );
       const firstOption = field("service").childNodes[0];
       expect(firstOption.value).toEqual("");
     })
 
     it("list all salon services", () => {
-      const services = ["Cut", "Blow-dry"];
+      
   
       render(
-        <AppointmentForm selectableServices={services} />
+        <AppointmentForm
+          original={blankAppointment} 
+          selectableServices={services} />
       );
   
       expect(
         labelsOfAllOptions(field("service"))
       ).toEqual(expect.arrayContaining(services));
     })
+
+    it("pre-selects the existing value", () => {
+      const appointment = {service: "Blow-dry"};
+
+      render (
+        <AppointmentForm
+          selectableServices={services}
+          original={appointment}
+        />
+      );
+      const option = findOption(
+        field("service"), 
+        "Blow-dry"
+      );
+      expect(option.selected).toBe(true);
+    })
   })
+
+  describe("time slot table", () => {
+    it("renders a table for time slots with an id", () => {
+      render(
+        <AppointmentForm original={blankAppointment}/>
+      );
+      expect(element("table#time-slots")).not.toBeNull();
+    });
+
+    it("renders a time slot for every half an hour between open and close times" , () => {
+      render(
+        <AppointmentForm
+          original={blankAppointment}
+          salonOpensAt={9}
+          salonClosesAt={11}
+        />
+      );
+  
+      const timeofDayHeadings = elements("tbody >* th");
+      expect(timeofDayHeadings[0]).toContainText(
+        "09:00"
+      );
+      expect(timeofDayHeadings[1]).toContainText(
+        "09:30"
+      );
+      expect(timeofDayHeadings[3]).toContainText(
+        "10:30"
+      );
+    })
+  });
+
+ 
 })
 
